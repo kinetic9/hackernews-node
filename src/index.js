9b10b99d8aace6fd77bb675fee5fc9f024c9266a
Bongani Khoza <bonganebnk@gmail.com>
2019-06-18 12:22:28 +0200

    Create GraphQlServer with basic Schema and Resolver

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
+// Dependancies
 const { GraphQLServer } = require('graphql-yoga');
 
+//GraphQL Schema, (!) defines a required field {Cannot be null}
+const typeDefs = `
+type Query {
+    info: String!
+}
+`
+
+// GraphQL implementation
+const resolvers = {
+    Query: {
+        info: () => {
+            `This is the API of a Hackernews Clone`
+        }
+    }
+}
+
+// bundle schema and resolvers into GraphQL server
+const server = new GraphQLServer ({
+    typeDefs,
+    resolvers,
+});
+
+
+// start the server, callback a console log
+server.start(()=> console.log(`Server is running on http://localhost:4000`));
