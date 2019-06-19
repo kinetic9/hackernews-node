0b534108e2e85d9a510d3119633555c9e06151e6
Bongani Khoza <bonganebnk@gmail.com>
2019-06-19 13:52:47 +0200

    +subcriptions

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
 
 
 // GraphQL implementation
@@ -16,7 +17,8 @@ const resolvers = {
     Query,
     Link,
     User,
-    Mutation    
+    Mutation,
+    Subscription    
     
 }
 
