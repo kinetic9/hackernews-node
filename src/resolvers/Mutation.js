29d361c4e8dc0c96f816b128ebaa9098e74ed297
Bongani Khoza <bonganebnk@gmail.com>
2019-06-19 11:40:34 +0200

    +auth mutations

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,50 @@
+async function signup(parent, args, context, info){
+    // use the bcrypt lib to hash the password given, using a salt of 10
+    const password = await bcrypt.hash(args.password, 10);
+
+    // use createUser from prisma's lib taking the args and the hashed password
+    const user = await context.prisma.createUser({...args, password });
+
+    // create a jwt token signed with the user's id
+    const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+    //return an object with the generated token and user variables
+    return {
+        token,
+        user
+    }
+}
+
+async function login (parent, args, context, info){
+
+    //
+    const user = await context.prisma.user({ email: args.email });
+    if (!user){
+        throw new Error ('No such user found')
+    }
+
+
+    //
+    const valid = await bcrypt.compare(args.password, user.password)
+    if(!valid){
+        throw new Error("Invalid password")
+    }
+
+
+    const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+    //return an object with token ad user variable
+    return{
+        token,
+        user
+    }
+
+}
+
+// Export the module
+
+module.exports = {
+    signup,
+    login,
+    post
+}
